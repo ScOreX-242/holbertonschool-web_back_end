d764d950bda022a913592ae694f200e321bc93f8
Amin Israfilov <israfilovamin@mail.ru>
2026-05-21 12:52:12 +0400

    Create 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module writes type-annotated function to_str that takes
+a float n as argument and returns the string representation of the float."""
+
+import math
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float."""
+    return str(n)
