5f4ad077f78d9d64a229cc85b86b126022a3e3d4
Amin Israfilov <israfilovamin@mail.ru>
2026-05-21 13:08:07 +0400

    Update 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module writes type-annotated function make_multiplier that takes a float multiplier
+This module writes type-annotated function make_multiplier that takes float multiplier
 as argument and returns a function that multiplies a float by multiplier.
 """
 
