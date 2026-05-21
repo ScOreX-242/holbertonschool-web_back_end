9aff748f4d62c6bb5c3b64e228b2ed355b7b504f
Amin Israfilov <israfilovamin@mail.ru>
2026-05-21 13:05:37 +0400

    Update 7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,6 +6,7 @@ an int OR float v as arguments
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple"""
     return (k, float(v ** 2))
