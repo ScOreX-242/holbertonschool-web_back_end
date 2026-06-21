cc97b878f6519746b3b3b44db59b8af5f8d7a480
Amin Israfilov <israfilovamin@mail.ru>
2026-06-21 23:25:32 +0400

    Remove blank line in 8-all.py
    Removed unnecessary blank line at the start of the file.

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 " Python function that lists all documents in a collection "
 
-
 def list_all(mongo_collection):
     """Return a list of all documents."""
     return list(mongo_collection.find())
