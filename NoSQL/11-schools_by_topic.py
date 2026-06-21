80f96495d8e69a0f7513ea4a0784be681a67b183
Amin Israfilov <israfilovamin@mail.ru>
2026-06-21 23:28:22 +0400

    Create 11-schools_by_topic.py

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+" Python function that returns the list of school having a specific topic "
+
+def schools_by_topic(mongo_collection, topic):
+  "Return list of schools having a specific topic."
+  return list(mongo_collection.find({"topics": topic}))
